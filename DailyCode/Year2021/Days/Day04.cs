ae59f0782571ef51f4fd88352e47b401f4a7a749
Alex Hebbelinck <alexhebbelinck@gmail.com>
2021-12-03 22:37:44 +0100

    Prepare Day 04

---
+++ DailyCode/Year2021/Days/Day04.cs
@@ -0,0 +1,28 @@
+﻿using DailyCode.Base;
+
+namespace DailyCode.Year2021.Days
+{
+    public class Day04 : BaseDay
+    {
+        //private List<(string command, int units)> _fileInput = new();
+
+        public Day04(string sessionId) : base(sessionId)
+        {
+        }
+
+        protected override void ExtractData(List<string> fileInput)
+        {
+            //_fileInput = fileInput
+        }
+
+        protected override string RunPart1()
+        {
+            return "";
+        }
+
+        protected override string RunPart2()
+        {
+            return "";
+        }
+    }
+}
