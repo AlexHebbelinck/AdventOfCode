ec0b9329243dd290c5018c3c9420f5790b2e887e
Alex Hebbelinck <alexhebbelinck@gmail.com>
2021-12-05 17:51:21 +0100

    Day 05 Part 1 and part 2

--- DailyCode/Year2021/Days/Day05.cs
+++ DailyCode/Year2021/Days/Day05.cs
@@ -1,10 +1,12 @@
 ﻿using DailyCode.Base;
+using DailyCode.Year2021.Models;
+using System.Text.RegularExpressions;
 
 namespace DailyCode.Year2021.Days
 {
     public class Day05 : BaseDay
     {
-        private List<int> _fileInput = new();
+        private List<LineCoordinates> LinesCoordinates { get; set; } = new();
 
         public Day05(string sessionId) : base(sessionId)
         {
@@ -12,17 +14,41 @@ namespace DailyCode.Year2021.Days
 
         protected override void ExtractData(List<string> fileInput)
         {
-         //   _fileInput = fileInput.ConvertAll(x => int.Parse(x));
+            foreach (var line in fileInput.Where(x => !string.IsNullOrWhiteSpace(x)))
+            {
+                var match = Regex.Match(line, @"(\d+),(\d+) -> (\d+),(\d+)");
+                LinesCoordinates.Add(new LineCoordinates
+                {
+                    From = (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value)),
+                    To = (int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value)),
+                });
+            }
         }
 
         protected override string RunPart1()
         {
-            return "";
+            return Run(true);
         }
 
         protected override string RunPart2()
         {
-            return "";
+            return Run();
+        }
+
+        private string Run(bool checkIfDiagonal = false)
+        {
+            var diagram = new int[LinesCoordinates.Max(x => x.HighestXCoordinate) + 1, LinesCoordinates.Max(y => y.HighestYCoordinate) + 1];
+
+            foreach (var lineCoordinates in LinesCoordinates.Where(x => !checkIfDiagonal || x.IsDiagonal))
+            {
+                var fullLineRange = lineCoordinates.GetFullLineRange();
+                foreach ((int x, int y) in fullLineRange)
+                {
+                    ++diagram[x, y];
+                }
+            }
+
+            return diagram.Cast<int>().Count(x => x > 1).ToString();
         }
     }
 }
---
+++ DailyCode/Year2021/Models/LineCoordinates.cs
@@ -0,0 +1,48 @@
+﻿namespace DailyCode.Year2021.Models
+{
+    public class LineCoordinates
+    {
+        public (int x, int y) From { get; set; }
+        public (int x, int y) To { get; set; }
+
+        public bool IsDiagonal
+            => From.x != To.x && From.y != To.y;
+
+        public int HighestXCoordinate
+            => Math.Max(From.x, To.x);
+
+        public int HighestYCoordinate
+            => Math.Max(From.y, To.y);
+
+        public List<(int x, int y)> GetFullLineRange()
+        {
+            if (IsDiagonal)
+                return GetFullDiagonalLineRange();
+
+            if (From.x != To.x)
+                return GetRange(From.x, To.x).Select(x => (x, From.y)).ToList();
+
+            return GetRange(From.y, To.y).Select(y => (From.x, y)).ToList();
+        }
+
+        private IEnumerable<int> GetRange(int fromPos, int toPos)
+        {
+            var minPos = Math.Min(fromPos, toPos);
+            var maxPos = Math.Max(fromPos, toPos);
+
+            var range = Enumerable.Range(minPos, (maxPos - minPos) + 1);
+            return fromPos < toPos
+                ? range.Reverse()
+                : range;
+        }
+
+        private List<(int x, int y)> GetFullDiagonalLineRange()
+        {
+            var xRanges = GetRange(From.x, To.x).ToList();
+            var yRanges = GetRange(From.y, To.y).ToList();
+
+            return xRanges.Join(yRanges, x => xRanges.IndexOf(x), y => yRanges.IndexOf(y), (x, y) => (x, y)).ToList();
+        }
+
+    }
+}
