a8cd16c36664f598d09f2be8c6ab0518b6525b4f
Alex Hebbelinck <alexhebbelinck@gmail.com>
2021-12-06 19:59:32 +0100

    Day 07

---
+++ DailyCode/Year2021/Days/Day07.cs
@@ -0,0 +1,28 @@
+﻿using DailyCode.Base;
+
+namespace DailyCode.Year2021.Days
+{
+    public class Day07 : BaseDay
+    {
+        //private List<int> _fileInput = new();
+
+        public Day07(string sessionId) : base(sessionId)
+        {
+        }
+
+        protected override void ExtractData(List<string> fileInput)
+        {
+            //_fileInput = fileInput.ConvertAll(x => int.Parse(x));
+        }
+
+        protected override string RunPart1()
+        {
+            return "";
+        }
+
+        protected override string RunPart2()
+        {
+            return "";
+        }
+    }
+}
