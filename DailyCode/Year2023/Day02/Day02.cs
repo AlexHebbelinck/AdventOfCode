b2fa0e5da255bb0936894eeb8ad16de1cb955495
Alex Hebbelinck <alexhebbelinck@gmail.com>
2023-12-01 22:01:10 +0100

    Setup day 02

---
+++ DailyCode/Year2023/Day02/Day02.cs
@@ -0,0 +1,22 @@
+﻿using DailyCode.Base;
+
+namespace DailyCode.Year2023.Day02
+{
+    public class Day02(string sessionId) : BaseDay(sessionId)
+    {
+        protected override void SetupData(List<string> fileInputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string RunPart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string RunPart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
