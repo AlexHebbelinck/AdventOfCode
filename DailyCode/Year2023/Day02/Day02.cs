06558b257d9964e78ad400ea3ba730e652a3dd97
Alex Hebbelinck <alehebbelinck@deloitte.com>
2023-12-02 11:53:46 +0100

    Day 02

--- DailyCode/Year2023/Day02/Day02.cs
+++ DailyCode/Year2023/Day02/Day02.cs
@@ -1,22 +1,55 @@
 ﻿using DailyCode.Base;
+using System.Drawing;
+using System.Text.RegularExpressions;
 
 namespace DailyCode.Year2023.Day02
 {
     public class Day02(string sessionId) : BaseDay(sessionId)
     {
+        private readonly List<Game> _games = [];
+
         protected override void SetupData(List<string> fileInputs)
         {
-            throw new NotImplementedException();
+            var gameRgx = new Regex("Game (?<GameId>\\d*):");
+            var subsetRgx = new Regex("\\d+ .*?(?=;|$)");
+            fileInputs.ForEach(fileLine =>
+            {
+                var game = new Game(int.Parse(gameRgx.Match(fileLine).Groups["GameId"].Value));
+                foreach (Match match in subsetRgx.Matches(fileLine))
+                {
+                    var cubesAmount = match.Value.Split(',');
+                    game.Sets.Add(new Set
+                    {
+                        RevealedCubes = cubesAmount.Select(cubeAmount => {
+                            cubeAmount = cubeAmount.Trim();
+                            string[] splittedString = cubeAmount.Split(' ');
+                            return new Cube(splittedString[1], int.Parse(splittedString[0])); 
+                        }).ToList()
+                    });
+                }
+
+                _games.Add(game);
+            });
         }
 
         protected override string RunPart1()
         {
-            throw new NotImplementedException();
+            List<(string Color, int MaxAmount)> maxPerColors =
+            [
+                ("red", 12),
+                ("blue", 14),
+                ("green", 13),
+            ];
+
+            return _games
+                .Where(x => x.IsValid(maxPerColors)).Select(x => x.Id)
+                .Sum()
+                .ToString();
         }
 
         protected override string RunPart2()
-        {
-            throw new NotImplementedException();
-        }
+            => _games.Select(x => x.GetLowestPerColor().Select(c => c.Amount).Aggregate((curr, next) => curr * next))
+                .Sum()
+                .ToString();
     }
 }
---
+++ DailyCode/Year2023/Day02/Game.cs
@@ -0,0 +1,55 @@
+﻿namespace DailyCode.Year2023.Day02
+{
+    public class Game
+    {
+        public Game(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+
+        public List<Set> Sets { get; set; } = [];
+
+        public List<(string Color, int Amount)> GetLowestPerColor()
+            => Sets.SelectMany(x => x.GetTotal())
+                .GroupBy(x => x.Color)
+                .Select(x => (Color: x.Key, Amount: x.Max(s => s.Amount)))
+                .ToList();
+
+        public bool IsValid(List<(string Color, int MaxAmount)> maxPerColors)
+        {
+            var isValid = true;
+            foreach (var (Color, MaxAmount) in maxPerColors)
+            {
+                if (Sets.Exists(x => x.GetTotal().Exists(x => x.Color.Equals(Color, StringComparison.InvariantCultureIgnoreCase) && x.Amount > MaxAmount)))
+                    isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+
+    public class Set
+    {
+        public List<Cube> RevealedCubes { get; set; } = [];
+
+        public List<(string Color, int Amount)> GetTotal()
+            => RevealedCubes.GroupBy(x => x.Color)
+                .Select(x => (Color: x.Key, Amount: x.Select(x => x.Amount).Sum()))
+                .ToList();
+    }
+
+    public class Cube
+    {
+        public Cube(string color, int amount)
+        {
+            Color = color;
+            Amount = amount;
+        }
+
+        public string Color { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
