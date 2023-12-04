c0199a5253128af2549d744a4596cd90a239f8b8
Alex Hebbelinck <alehebbelinck@deloitte.com>
2023-12-04 19:53:33 +0100

    Setup day 4

---
+++ DailyCode/Year2023/Day04/Day04.cs
@@ -0,0 +1,22 @@
+﻿using DailyCode.Base;
+
+namespace DailyCode.Year2023.Day04
+{
+    public class Day04(string sessionId) : BaseDay(sessionId)
+    {
+        protected override void SetupData(List<string> fileInputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string RunPart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string RunPart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
